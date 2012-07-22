a80b65f0235b046716296eac863105148382ece2
Marsel Zaripov <marszaripov@gmail.com>
2012-07-22 14:46:26 +0400

    changed

--- src/ml_list.c
+++ src/ml_list.c
@@ -10,16 +10,20 @@ ml_list ml_list_new(size_t size)
 {
     size_t *obj = &size;
     ml_list sentinel = new_element(obj, sizeof(size_t));
+
     sentinel->next = sentinel;
     sentinel->prev = sentinel;
+
     return sentinel;
 }
 
 void ml_list_push_back(ml_list list, const void *obj)
 {
     size_t *size = (size_t *) list->object;
+
     ml_list new_elem = new_element(obj, *size);
     ml_list last_elem = list->prev;
+    
     last_elem->next = new_elem;
     new_elem->prev = last_elem;
     new_elem->next = list;
@@ -29,8 +33,10 @@ void ml_list_push_back(ml_list list, const void *obj)
 void ml_list_push_front(ml_list list, const void *obj)
 {
     size_t *size = (size_t *) list->object;
+
     ml_list new_elem = new_element(obj, *size);
     ml_list first_elem = list->next;
+
     first_elem->prev = new_elem;
     new_elem->next = first_elem;
     new_elem->prev = list;
@@ -42,6 +48,7 @@ void ml_list_pop_back(ml_list list)
     ml_list last = list->prev;
     list->prev = last->prev;
     last->prev->next = list;
+
     delete_element(last);
 }
 
@@ -50,6 +57,7 @@ void ml_list_pop_front(ml_list list)
     ml_list first = list->next;
     list->next = first->next;
     first->next->prev = list;
+
     delete_element(first);
 }
 
@@ -66,7 +74,7 @@ void ml_list_print(ml_list list)
 static ml_list new_element(const void *obj, size_t size)
 {
     ml_list list = (ml_list) malloc(sizeof(struct _ml_list));
-    list->object = malloc(size);    
+    list->object = malloc(size);
     memcpy(list->object, obj, size);
     return list;
 }
