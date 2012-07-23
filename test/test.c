b65370f114ecb86846d81b85524a2d40e08ffbe7
Marsel Zaripov <marszaripov@gmail.com>
2012-07-23 11:48:44 +0400

    add new functions to test

--- test/test.c
+++ test/test.c
@@ -6,10 +6,21 @@ int main()
 {
     ml_list list = ml_list_new(sizeof(int));
     int i;
+
     for (i = 0; i < 100; i++) {
 	ml_list_push_front(list, &i);
     }
-    ml_list_print(list);
+    
+    if (ml_list_empty(list)) {
+	printf("empty\n");
+    } else {
+	printf("size: %d\n", (int) ml_list_size(list));
+	ml_list_clear(list);
+	if (ml_list_empty(list))
+	    printf("size: %d\n", (int) ml_list_size(list));
+    }
+    
+    ml_list_delete(&list);
 
     return 0;
 }
